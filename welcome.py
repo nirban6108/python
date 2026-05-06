acb86c3b9d89f18bb06e46975bce8ac7b5b7134a
nirban6108 <nirban6108@gmail.com>
2026-05-06 18:29:01 +0800

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,9 @@
+print("My Daily Routine")
+
+print("At 7 AM, I wake up and the start preparing to go to school")
+print("Then, I go to school at about 8 AM")
+print("I go to school for" ,2*3, "hours")
+print("After that, I might go to some activities for 2 to 3 hours depending on the day")
+print("After the activity, i study for 2 hours")
+print("Finally, after dinner, I go to sleep at around 9 PM")
+print("2*3")
